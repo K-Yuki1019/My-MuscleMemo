7231ca656881ea3cb1c440d61bd2a8848ba7dd16
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-10-07 17:49:49 +0900

    初期コミット

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireFunctionsModule, REGION} from '@angular/fire/functions';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +19,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firease),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
+    AngularFireFunctionsModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [{ provide: REGION, useValue: 'asia-northeast1' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firease: {
+    apiKey: 'AIzaSyA28joqQFGbf-ZasQqGe2Tui0dduT19Jk4',
+    authDomain: 'my-musclememo.firebaseapp.com',
+    databaseURL: 'https://my-musclememo.firebaseio.com',
+    projectId: 'my-musclememo',
+    storageBucket: 'my-musclememo.appspot.com',
+    messagingSenderId: '868606989849',
+    appId: '1:868606989849:web:49c272df18d33ba1963c88',
+    measurementId: 'G-JKN5E2ZPQG',
+  },
 };
 
 /*

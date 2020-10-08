0bf00e64919a475f942972f9f62ac4a2003fd484
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-10-08 17:08:54 +0900

    feat(welcome): Welcome画面作成

---
+++ src/app/welcome/welcome.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WelcomeRoutingModule } from './welcome-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+@NgModule({
+  declarations: [WelcomeComponent],
+  imports: [
+    CommonModule,
+    WelcomeRoutingModule,
+    MatButtonModule,
+    MatSnackBarModule
+  ]
+})
+export class WelcomeModule { }
---
+++ src/app/welcome/welcome/welcome.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private snackBar: MatSnackBar
+    ) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.authService.login();
+    this.router.navigateByUrl('');
+  }
+
+  logout(){
+    this.authService.logout();
+    this.router.navigateByUrl('');
+  }
+}

2bc2fb42532816810249987b51046b286a2f06f5
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-10-08 17:09:24 +0900

    feat(firebase): Firebase deploy完了

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firease: {
+    apiKey: 'AIzaSyA28joqQFGbf-ZasQqGe2Tui0dduT19Jk4',
+    authDomain: 'my-musclememo.firebaseapp.com',
+    databaseURL: 'https://my-musclememo.firebaseio.com',
+    projectId: 'my-musclememo',
+    storageBucket: 'my-musclememo.appspot.com',
+    messagingSenderId: '868606989849',
+    appId: '1:868606989849:web:49c272df18d33ba1963c88',
+    measurementId: 'G-JKN5E2ZPQG',
+  },
 };

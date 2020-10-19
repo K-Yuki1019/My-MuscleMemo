bf3b7de5f5507ddb28a901a901dddc5a756e75ca
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-10-19 21:01:21 +0900

    feat(function): エラー修正(暫定)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,4 +3,15 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
-export { createUser } from './user';
+export const db = admin.firestore();
+export const createUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate((user) => {
+    const userData = {
+      uid: user.uid,
+      userName: user.displayName,
+      avatarUrl: user.photoURL,
+    };
+    return db.doc(`users/${user.uid}`).set(userData);
+  });

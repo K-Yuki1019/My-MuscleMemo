874ed1c51f64f448e2c99bf651b824004b1f84ea
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-10-23 08:07:50 +0900

    feat(card-list): カードリストのマークアップ

--- src/app/home/card-list/card-list.component.ts
+++ src/app/home/card-list/card-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from 'src/app/services/note.service';
+import { Observable } from 'rxjs';
+import { Note } from 'src/app/interfaces/note';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.scss']
+  styleUrls: ['./card-list.component.scss'],
 })
 export class CardListComponent implements OnInit {
+  notes$: Observable<Note[]> = this.noteService.getNotesWithUsers();
+  user$: Observable<User> = this.authService.user$;
 
-  constructor() { }
+  constructor(
+    private noteService: NoteService,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

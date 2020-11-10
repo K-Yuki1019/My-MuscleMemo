641ab8beb2b54e8fdc389a09f1ed172b510c520c
隈本優希 <morikuma1019@kumamotoyuukinoMacBook-Air.local>
2020-11-10 19:15:59 +0900

    fix(Card): カード修正

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,19 @@ import { HomeComponent } from './home/home.component';
 import { CardComponent } from './card/card.component';
 import { CardListComponent } from './card-list/card-list.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [HomeComponent, CardComponent, CardListComponent],
-  imports: [CommonModule, HomeRoutingModule, MatDialogModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
 })
 export class HomeModule {}

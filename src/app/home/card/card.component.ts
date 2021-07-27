5bf16c240db1b9c20e932d7a946a7fb3680f4a20
K-Yuki1019 <63516648+K-Yuki1019@users.noreply.github.com>
2021-07-27 14:09:22 +0900

    fix(card): cardのUIを変更

--- src/app/home/card/card.component.ts
+++ src/app/home/card/card.component.ts
@@ -2,13 +2,17 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as faker from 'faker/locale/ja';
 import { Chart } from 'chart.js';
 import { _MatRadioGroupBase } from '@angular/material/radio';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent implements OnInit, AfterViewInit {
+export class CardComponent implements OnInit {
+  user$ = this.authService.user$;
+
   fakeNoteWithUser$ = new Array(100).fill(null).map(() => {
     return {
       createdAt: faker.date.month(),
@@ -23,83 +27,7 @@ export class CardComponent implements OnInit, AfterViewInit {
     };
   });
 
-  ctx = 'myChart';
-  myChart: Chart;
-
-  ngAfterViewInit() {
-    this.myChart = new Chart(this.ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          '1月',
-          '2月',
-          '3月',
-          '4月',
-          '5月',
-          '6月',
-          '7月',
-          '8月',
-          '9月',
-          '10月',
-          '11月',
-          '12月',
-        ],
-        datasets: [
-          {
-            label: '体重',
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            data: [59, 60, 73, 52, 49, 50, 59, 60, 73, 52, 49, 50],
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-          },
-          {
-            label: '体脂肪率',
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            data: [13, 13, 17, 13, 15, 14, 14, 15, 17, 14, 15, 13],
-            borderColor: 'rgba(255, 206, 86, 1)',
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'Chart.js Line Chart',
-        },
-        tooltips: {
-          mode: 'index',
-          intersect: false,
-        },
-        hover: {
-          mode: 'nearest',
-          intersect: true,
-        },
-        scales: {
-          xAxes: [
-            {
-              display: true,
-              scaleLabel: {
-                display: true,
-                labelString: 'Month',
-              },
-            },
-          ],
-          yAxes: [
-            {
-              display: true,
-              scaleLabel: {
-                display: true,
-                labelString: 'Value',
-              },
-            },
-          ],
-        },
-      },
-    });
-  }
-
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 }

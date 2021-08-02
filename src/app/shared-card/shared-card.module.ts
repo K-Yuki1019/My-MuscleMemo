2baf2056f4d40937196e923f16f3db56f8e58b7b
K-Yuki1019 <63516648+K-Yuki1019@users.noreply.github.com>
2021-08-02 23:28:53 +0900

    change(Detail): 詳細画面の構造を変更＃１

--- src/app/shared-card/shared-card.module.ts
+++ src/app/shared-card/shared-card.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedCardComponent } from './shared-card/shared-card.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SharedCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [SharedCardComponent],
 })
 export class SharedCardModule {}
